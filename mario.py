d538fd5fe1910949487be3737cb62ee74050cb2a
williamhenriksson <151830610+williamhenriksson@users.noreply.github.com>
2024-03-04 13:44:43 +0100

    commit

--- mario.py
+++ mario.py
@@ -7,10 +7,25 @@ screen_height = 700
 
 screen = pygame.display.set_mode((screen_width, screen_height))
 
+player = pygame.Rect((300, 250, 50, 50))
+
+run = True
+
+while run:
+
+    pygame.draw.rect(screen, (255, 255, 255), player)
+    key = pygame.key.get_pressed()
+    if key[pygame.K_a] == True:
+    for event in pygame.event.get():
+        if event.type == event.QUIT:
+            run = false
+    pygame.display.update()
+
 
 # Jump funktion
 
 # Movement should contain: move right, move left och run
+
 
 # Collisions: with blocks and enemies
 
@@ -21,3 +36,6 @@ screen = pygame.display.set_mode((screen_width, screen_height))
 # Character, Mario
 
 # Powerups, mushroom
+
+
+pygame.quit()
