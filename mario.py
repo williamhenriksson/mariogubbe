41856d6d000a21ad9971ab62c92e0367fa75af9d
williamhenriksson <151830610+williamhenriksson@users.noreply.github.com>
2024-03-04 13:06:37 +0100

    Initial commit

---
+++ mario.py
@@ -0,0 +1,23 @@
+import pygame
+
+pygame.init()
+
+screen_width = 800
+screen_height = 700
+
+screen = pygame.display.set_mode((screen_width, screen_height))
+
+
+# Jump funktion
+
+# Movement should contain: move right, move left och run
+
+# Collisions: with blocks and enemies
+
+# Enemies: Goomba
+
+# Map, blocks and background drawings
+
+# Character, Mario
+
+# Powerups, mushroom
