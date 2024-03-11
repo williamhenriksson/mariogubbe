d50c1a34b10a1b5f27c4492fc51fa1783fe6a7cf
williamhenriksson <151830610+williamhenriksson@users.noreply.github.com>
2024-03-11 08:32:55 +0100

    commit

--- mario.py
+++ mario.py
@@ -17,6 +17,7 @@ screen = pygame.display.set_mode((screen_width, screen_height))
 #x oxh y koordinater
 X_POSITION, Y_POSITION = 400, 660
 
+# Så att mario står där han ska när han blir inlagd
 mario_rect = Standing_ground.get_rect(center=(X_POSITION, Y_POSITION))
 
 # vad som gör så att marios hopp blir bra
@@ -24,7 +25,7 @@ Y_gravity = 0.7
 Jump_height = 15
 Y_velocity = 0
 
-
+# run är sant så spelet körs
 run = True
 
 #rörelse
@@ -42,20 +43,22 @@ while run:
         if mario_rect.y == 250:
             Y_velocity = -Jump_height
 
+    # Hur mario åker upp i luften
     Y_velocity += Y_gravity
     mario_rect.y += Y_velocity
 
     if mario_rect.y > 250:
         mario_rect.y = 250
         Y_velocity = 0
-
+    # gör så att rätt bild på mario blir inlagd när
     if mario_rect.y == 250:
         screen.blit(Standing_ground, mario_rect)
+    # Annars ska mariobilden vara hopp bilden
     else:
         screen.blit(Jumping_ground, mario_rect)
 
     pygame.display.update()
-
+    # så att pygame avslutas
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
