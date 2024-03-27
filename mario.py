7205b0eba5cff1c6fdb5130945332a0c98c1adc6
williamhenriksson <151830610+williamhenriksson@users.noreply.github.com>
2024-03-27 09:40:42 +0100

    commit

--- mario.py
+++ mario.py
@@ -1,20 +1,24 @@
 import pygame
 
-
 # initiera pygame
 pygame.init()
-
-#laddar in bilderna som används till spelet
-Standing_ground = pygame.transform.scale(pygame.image.load("C:/Users/WilliamHenriksson/PycharmProjects/pythonProject4/mario.png"), (48, 64))
-Jumping_ground = pygame.transform.scale(pygame.image.load("C:/Users/WilliamHenriksson/PycharmProjects/PythonProject4/Jumping_mario.png"), (48, 64))
-Background = pygame.transform.scale(pygame.image.load("C:/Users/WilliamHenriksson/PycharmProjects/PythonProject4/Mario_background.jpg"), (800, 361))
+# __ENEMY IMAGE__
+enemy_image = pygame.transform.scale(
+    pygame.image.load("enemy.png"), (80, 64))
+# laddar in bilderna som används till spelet
+Standing_ground = pygame.transform.scale(
+    pygame.image.load("mario.png"), (48, 64))
+Jumping_ground = pygame.transform.scale(
+    pygame.image.load("Jumping_mario.png"), (64, 64))
+Background = pygame.transform.scale(
+    pygame.image.load("Mario_background.jpg"), (800, 361))
 # klocka så att skärmen refreshas
 CLOCK = pygame.time.Clock()
-#hur stor skärmen ska vara
+# hur stor skärmen ska vara
 screen_width = 800
 screen_height = 362
 screen = pygame.display.set_mode((screen_width, screen_height))
-#x oxh y koordinater
+# x oxh y koordinater
 X_POSITION_mario, Y_POSITION_mario = 400, 660
 
 # Så att mario står där han ska när han blir inlagd
@@ -28,9 +32,10 @@ Y_velocity = 0
 # run är sant så spelet körs och bilderna kommer upp på skärmen
 run = True
 
-#rörelse
+# rörelse
 while run:
     screen.blit(Background, (0, 0))
+    screen.blit(enemy_image, (100, 100))
 
     # kollar vilka tangenter som blivit och blir tryckta
     key = pygame.key.get_pressed()
@@ -39,7 +44,7 @@ while run:
         mario_rect.x -= 5
     if key[pygame.K_d]:
         mario_rect.x += 5
-    #rörelse för hopp
+    # rörelse för hopp
     if key[pygame.K_SPACE]:
         if mario_rect.y == 250:
             Y_velocity = -Jump_height
@@ -63,7 +68,7 @@ while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
-    #vilken framerate spelet ska ha
+    # vilken framerate spelet ska ha
     CLOCK.tick(60)
     print(mario_rect.x)
     """Border Collision"""
@@ -71,6 +76,5 @@ while run:
         mario_rect.x = 0
     if mario_rect.x >= 750:
         mario_rect.x = 750
-
 
 pygame.quit()
