a5aa65b5133b6babfc567dbef48db9a703c869a4
williamhenriksson <151830610+williamhenriksson@users.noreply.github.com>
2024-05-15 13:37:04 +0200

    commit

--- mario.py
+++ mario.py
@@ -2,6 +2,8 @@ import pygame
 
 # initiera pygame
 pygame.init()
+# Coin bild
+Coin_image = pygame.transform.scale(pygame.image.load("coin-1.png"), (30, 30))
 # __ENEMY IMAGE__
 enemy_image = pygame.transform.scale(pygame.image.load("enemy.png"), (80, 64))
 # laddar in bilderna som används till spelet
@@ -20,9 +22,17 @@ Game_over_text = pygame.transform.scale(pygame.image.load("game over text.png"),
 # klocka så att skärmen refreshas
 CLOCK = pygame.time.Clock()
 
+# movement
+Walking_speed = 5
 # hur mario bilden ska försvinna när man dör
 
 Transparent = (0, 0, 0, 0)
+
+# border edges
+Border_edge_l = 0
+Border_edge_r = 750
+Goomba_edge_l = -60
+Goomba_edge_r = 800
 # hur stor skärmen ska vara
 
 screen = pygame.display.set_mode((800, 362))
@@ -30,7 +40,7 @@ screen = pygame.display.set_mode((800, 362))
 # x oxh y koordinater
 X_POSITION_MARIO, Y_POSITION_MARIO = 400, 362
 X_POSITION_ENEMY, Y_POSITION_ENEMY = 700, 300
-
+Coin_spawn_x, Coin_spawn_y = 500, 130
 # Så att mario och enemy står där han ska när han blir inlagd
 mario_rect = Standing_ground.get_rect(center=(X_POSITION_MARIO, Y_POSITION_MARIO))
 Enemy_rect = enemy_image.get_rect(center=(X_POSITION_ENEMY, Y_POSITION_ENEMY))
@@ -39,73 +49,87 @@ Death_image_rect = Standing_ground.get_rect(center=(X_POSITION_MARIO, Y_POSITION
 Y_gravity = 0.7
 Jump_height = 15
 Y_velocity = 0
+Mario_max_height = 250
+# mario död bool så det blir enklare för han att sluta fungera när han dör
+Mario_dead = False
+
+
+def handle_game_over(Mario_dead, key, mario_rect, Enemy_rect, direction):
+    if Mario_dead:
+        key = None
+        mario_rect.x = mario_rect.x
+        Enemy_rect.x = Enemy_rect.x
+        direction = 0
+    return key, mario_rect, Enemy_rect, direction
+
 
 # run är sant så spelet körs och bilderna kommer upp på skärmen
 run = True
 
 # konstant till att någon rör säg i en speciell hastighet
+
 direction = -1
 # hela run loopen så spelet går
 while run:
+    # kollar vilka tangenter som blivit och blir tryckta
+    key = pygame.key.get_pressed()
+
+    key, mario_rect, Enemy_rect, direction = handle_game_over(Mario_dead, key, mario_rect, Enemy_rect, direction)
     # Så att bilder på bakgrunden och enemy uppdateras
     screen.blit(Background, (0, 0))
     screen.blit(enemy_image, (Enemy_rect.x, Enemy_rect.y))
+    screen.blit(Coin_image, (Coin_spawn_x, Coin_spawn_y))
+    # Justerar spelarens position efter vilka tangenter som trycks
+    if key[pygame.K_a]:
+        mario_rect.x -= Walking_speed
+    if key[pygame.K_d]:
+        mario_rect.x += Walking_speed
+    # rörelse för hopp (mario)
+    if key[pygame.K_SPACE]:
+        if mario_rect.y == Mario_max_height:
+            Y_velocity = -Jump_height
+
+    # Så att mario inte bara flyger iväg när nam hoppar och att han flyger upp som han ska
+    Y_velocity += Y_gravity
+    mario_rect.y += Y_velocity
+
+    # Sätter en maxhöjd på marios hopp och när den uppnås slutar han gå uppåt
+    if mario_rect.y > Mario_max_height:
+        mario_rect.y = Mario_max_height
+        Y_velocity = 0
+
+    # gör så att rätt bild på mario blir inlagd när
+    if mario_rect.y == Mario_max_height:
+        screen.blit(Standing_ground, mario_rect)
+
+    # Annars ska mariobilden vara hopp bilden
+    else:
+        screen.blit(Jumping_ground, mario_rect)
 
     # så att enemy rör sig i directions hastighet vilket är -1
     Enemy_rect.x += direction
     # så att gubben rör sig tills hans x kordinat är -60 i -1 i direction
-    if Enemy_rect.x >= -60:
+    if Enemy_rect.x >= Goomba_edge_l:
         direction = -1
     else:
-        Enemy_rect.x = 800
+        Enemy_rect.x = Goomba_edge_r
+
+    # Border Collision
+    if mario_rect.x < Border_edge_l:
+        mario_rect.x = Border_edge_l
+    if mario_rect.x >= Border_edge_r:
+        mario_rect.x = Border_edge_r
 
     # Så allt försvinner förutom mario och enemy när man dör
     # så att game over skärmen kommer upp när man dör
-    if mario_rect.x == Enemy_rect.x - 18 and mario_rect.y != Enemy_rect.y + 5:
+    if Enemy_rect.x + 20 >= mario_rect.x >= Enemy_rect.x - 20 and Enemy_rect.y + 30 >= mario_rect.y >= Enemy_rect.y - 40:
+        Mario_dead = True
         Standing_ground.fill(Transparent)
         Background.fill(Transparent)
         direction = 0
         screen.blit(Death_image, mario_rect)
         screen.blit(Game_over, (800, 362))
         screen.blit(Game_over_text, (0, 0))
-    elif mario_rect.x == Enemy_rect.x + 25 and mario_rect.y != Enemy_rect.y + 5:
-        Standing_ground.fill(Transparent)
-        Background.fill(Transparent)
-        direction = 0
-        screen.blit(Death_image, mario_rect)
-        screen.blit(Game_over, (800, 362))
-        screen.blit(Game_over_text, (0, 0))
-
-# kollar vilka tangenter som blivit och blir tryckta
-    key = pygame.key.get_pressed()
-# Justerar spelarens position efter vilka tangenter som trycks
-    if key[pygame.K_a]:
-        mario_rect.x -= 4
-    if key[pygame.K_d]:
-        mario_rect.x += 4
-
-# rörelse för hopp ( mario )
-    if key[pygame.K_SPACE]:
-        if mario_rect.y == 250:
-            Y_velocity = -Jump_height
-
-# Så att mario inte bara flyger iväg när nam hoppar och att han flyger upp som han ska
-    Y_velocity += Y_gravity
-    mario_rect.y += Y_velocity
-
-# Sätter en maxhöjd på marios hopp och när den uppnås slutar han gå uppåt
-    if mario_rect.y > 250:
-        mario_rect.y = 250
-        Y_velocity = 0
-
-# gör så att rätt bild på mario blir inlagd när
-    if mario_rect.y == 250:
-        screen.blit(Standing_ground, mario_rect)
-
-# Annars ska mariobilden vara hopp bilden
-    else:
-        screen.blit(Jumping_ground, mario_rect)
-
 # Så att displayen uppdateras och bilderna inte bara är där
     pygame.display.update()
 # så att pygame avslutas
@@ -115,10 +139,5 @@ while run:
 # vilken framerate spelet ska ha
     CLOCK.tick(60)
 print(mario_rect.x)
-"""Border Collision"""
-if mario_rect.x < 0:
-    mario_rect.x = 0
-if mario_rect.x >= 750:
-    mario_rect.x = 750
 
 pygame.quit()
--- test.py
+++ test.py
@@ -1,124 +1,143 @@
 import pygame
 
-
-class Enemy:
-    def __init__(self, x, y, width, height, image_path):
-        self.x = 700
-        self.y = 300
-        self.width = 80
-        self.height = 64
-        self.image = pygame.transform.scale(pygame.image.load("enemy.png"), (80, 64))
-        self.hitbox = (self.x, self.y, self.width, self.height)
-
-    def draw(self, screen):
-        screen.blit(self.image, (self.x, self.y))
 # initiera pygame
 pygame.init()
+# Coin bild
+Coin_image = pygame.transform.scale(pygame.image.load("coin-1.png"), (30, 30))
 # __ENEMY IMAGE__
-enemy_image = pygame.transform.scale(
-    pygame.image.load("enemy.png"), (80, 64))
+enemy_image = pygame.transform.scale(pygame.image.load("enemy.png"), (80, 64))
 # laddar in bilderna som används till spelet
-Standing_ground = pygame.transform.scale(
-    pygame.image.load("mario.png"), (48, 64))
-Jumping_ground = pygame.transform.scale(
-    pygame.image.load("Jumping_mario.png"), (64, 64))
-Background = pygame.transform.scale(
-    pygame.image.load("Mario_background.jpg"), (800, 361))
+# vanlig mario
+Standing_ground = pygame.transform.scale(pygame.image.load("mario.png"), (48, 64))
+# hoppande mario
+Jumping_ground = pygame.transform.scale(pygame.image.load("Jumping_mario.png"), (64, 64))
+# bakgrund
+Background = pygame.transform.scale(pygame.image.load("Mario_background.jpg"), (800, 361))
+# mario död
+Death_image = pygame.transform.scale(pygame.image.load("Mario_death.png"), (48, 64))
+# svart skärm
+Game_over = pygame.transform.scale(pygame.image.load("game-over.webp"), (800, 362))
+# game over text
+Game_over_text = pygame.transform.scale(pygame.image.load("game over text.png"), (800, 362))
 # klocka så att skärmen refreshas
 CLOCK = pygame.time.Clock()
+
+# movement
+Walking_speed = 5
+# hur mario bilden ska försvinna när man dör
+
+Transparent = (0, 0, 0, 0)
+
+# border edges
+Border_edge_l = 0
+Border_edge_r = 750
+Goomba_edge_l = -60
+Goomba_edge_r = 800
 # hur stor skärmen ska vara
 
 screen = pygame.display.set_mode((800, 362))
-screen_width = screen.get_width()
+
 # x oxh y koordinater
 X_POSITION_MARIO, Y_POSITION_MARIO = 400, 362
 X_POSITION_ENEMY, Y_POSITION_ENEMY = 700, 300
-
+Coin_spawn_x, Coin_spawn_y = 500, 130
 # Så att mario och enemy står där han ska när han blir inlagd
 mario_rect = Standing_ground.get_rect(center=(X_POSITION_MARIO, Y_POSITION_MARIO))
 Enemy_rect = enemy_image.get_rect(center=(X_POSITION_ENEMY, Y_POSITION_ENEMY))
-
+Death_image_rect = Standing_ground.get_rect(center=(X_POSITION_MARIO, Y_POSITION_MARIO))
 # vad som gör så att marios hopp åker upp och ner på rätt sätt
 Y_gravity = 0.7
 Jump_height = 15
 Y_velocity = 0
 
+# mario död bool så det blir enklare för han att sluta fungera när han dör
+Mario_dead = False
+
+
+def handle_game_over(Mario_dead, key, mario_rect, Enemy_rect, direction):
+    if Mario_dead:
+        key = None
+        mario_rect.x = mario_rect.x
+        Enemy_rect.x = Enemy_rect.x
+        direction = 0
+    return key, mario_rect, Enemy_rect, direction
+
+
 # run är sant så spelet körs och bilderna kommer upp på skärmen
 run = True
 
-# rörelse
+# konstant till att någon rör säg i en speciell hastighet
+
+direction = -1
+# hela run loopen så spelet går
 while run:
-    screen.blit(Background, (0, 0))
-    screen.blit(enemy_image, (Enemy_rect.x, Enemy_rect.y))
-
-    direction = -1
-
-    while run:
-        screen.blit(Background, (0, 0))
-        screen.blit(enemy_image, (Enemy_rect.x, Enemy_rect.y))
-
-    Enemy_rect.x += direction
-
-    if Enemy_rect.x >= -60:
-        direction = -1
-    else:
-        Enemy_rect.x = 800
-
-    if mario_rect.x == Enemy_rect.x - 18:
-        Standing_ground.fill(Transparent)
-        Background.fill(Transparent)
-        direction = 0
-        screen.blit(Death_image, mario_rect)
-        screen.blit(Game_over, (800, 362))
-        screen.blit(Game_over_text, (0, 0))
-    elif mario_rect.x == Enemy_rect.x + 25:
-        Standing_ground.fill(Transparent)
-        Background.fill(Transparent)
-        direction = 0
-        screen.blit(Death_image, mario_rect)
-        screen.blit(Game_over, (800, 362))
-        screen.blit(Game_over_text, (0, 0))
-
     # kollar vilka tangenter som blivit och blir tryckta
     key = pygame.key.get_pressed()
+
+    key, mario_rect, Enemy_rect, direction = handle_game_over(Mario_dead, key, mario_rect, Enemy_rect, direction)
+    # Så att bilder på bakgrunden och enemy uppdateras
+    screen.blit(Background, (0, 0))
+    screen.blit(enemy_image, (Enemy_rect.x, Enemy_rect.y))
+    screen.blit(Coin_image, (Coin_spawn_x, Coin_spawn_y))
     # Justerar spelarens position efter vilka tangenter som trycks
     if key[pygame.K_a]:
-        mario_rect.x -= 4
+        mario_rect.x -= Walking_speed
     if key[pygame.K_d]:
-        mario_rect.x += 4
-
-    # rörelse för hopp
+        mario_rect.x += Walking_speed
+    # rörelse för hopp (mario)
     if key[pygame.K_SPACE]:
         if mario_rect.y == 250:
             Y_velocity = -Jump_height
 
-    # Hur mario åker upp i luften
+    # Så att mario inte bara flyger iväg när nam hoppar och att han flyger upp som han ska
     Y_velocity += Y_gravity
     mario_rect.y += Y_velocity
 
+    # Sätter en maxhöjd på marios hopp och när den uppnås slutar han gå uppåt
     if mario_rect.y > 250:
         mario_rect.y = 250
         Y_velocity = 0
+
     # gör så att rätt bild på mario blir inlagd när
     if mario_rect.y == 250:
         screen.blit(Standing_ground, mario_rect)
+
     # Annars ska mariobilden vara hopp bilden
     else:
         screen.blit(Jumping_ground, mario_rect)
 
+    # så att enemy rör sig i directions hastighet vilket är -1
+    Enemy_rect.x += direction
+    # så att gubben rör sig tills hans x kordinat är -60 i -1 i direction
+    if Enemy_rect.x >= Goomba_edge_l:
+        direction = -1
+    else:
+        Enemy_rect.x = Goomba_edge_r
+
+    # Border Collision
+    if mario_rect.x < Border_edge_l:
+        mario_rect.x = Border_edge_l
+    if mario_rect.x >= Border_edge_r:
+        mario_rect.x = Border_edge_r
+
+    # Så allt försvinner förutom mario och enemy när man dör
+    # så att game over skärmen kommer upp när man dör
+    if Enemy_rect.x + 20 >= mario_rect.x >= Enemy_rect.x - 20 and Enemy_rect.y + 30 >= mario_rect.y >= Enemy_rect.y - 40:
+        Mario_dead = True
+        Standing_ground.fill(Transparent)
+        Background.fill(Transparent)
+        direction = 0
+        screen.blit(Death_image, mario_rect)
+        screen.blit(Game_over, (800, 362))
+        screen.blit(Game_over_text, (0, 0))
+# Så att displayen uppdateras och bilderna inte bara är där
     pygame.display.update()
-    # så att pygame avslutas
+# så att pygame avslutas
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
-    # vilken framerate spelet ska ha
+# vilken framerate spelet ska ha
     CLOCK.tick(60)
-    print(mario_rect.x)
-    """Border Collision"""
-    if mario_rect.x < 0:
-        mario_rect.x = 0
-    if mario_rect.x >= 750:
-        mario_rect.x = 750
+print(mario_rect.x)
 
 pygame.quit()
-
